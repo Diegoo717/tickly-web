5f631c2bee37e97d9e96f087a601d23f93c1ccac
Diego <soydiegoo71@gmail.com>
2025-12-05 19:43:14 -0600

    fix: update baseURL from api for production

--- src/services/api/axiosInstance.ts
+++ src/services/api/axiosInstance.ts
@@ -7,7 +7,7 @@ import axios, {
 import { supabase } from "../../lib/supabaseClient";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "http://tickly-api-prod.eba-twbmhd2g.us-east-1.elasticbeanstalk.com",
+  baseURL: "https://tickly-api-production.up.railway.app",
   timeout: 40000,
   headers: {
     "Content-Type": "application/json",
